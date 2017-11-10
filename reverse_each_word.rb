eacab57a5708cf30e63d8e90b5931e586973f5c7
Connor Gallopo <connor.gallopo@me.com>
2017-11-10 23:01:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,16 @@
 require "pry"
+# def reverse_each_word(sentence)
+#   new_sentence = []
+#   array = sentence.split
+#   array.each do |s|
+#     new_sentence << s.reverse
+#   end
+#   new_sentence.join(" ")
+# end
+
 def reverse_each_word(sentence)
-  new_sentence = []
   array = sentence.split
-  array.each do |s|
-    new_sentence << s.reverse
+    array.each do |s|
+      s.reverse
+    end
   end
-  new_sentence.join(" ")
-end
