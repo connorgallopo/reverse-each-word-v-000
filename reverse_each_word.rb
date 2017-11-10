ff5f229a0a3d65454871f69776329595cd9ae401
Connor Gallopo <connor.gallopo@me.com>
2017-11-10 22:47:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   new_sentence = []
   array = sentence.split
   array.each do |s|
-    new_sentence << s.reverse
-    new_sentence
+    sentence << s.reverse
+    sentence
   end
 end
